a4ff720a40d42038c09b532e7dbd700ae463a99a
«Alena» <mrk.alena@yandex.ru>
2023-09-28 23:58:38 +1000

    68.64.66

---
+++ 66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SUMofNat(m,n);
+
+void SUMofNat(int m, int n)
+{
+    Console.WriteLine(MtoN(m-1, n));
+}
+
+int MtoN(int m, int n)
+{
+    int result = m;
+    if (m==n)
+    return 0;
+    else
+    {
+        m++;
+        result = m + MtoN(m,n);
+        return result;
+    }
+}
---
+++ 68/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите положительное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите положительное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AF(m,n);
+
+void AF(int m, int n)
+{
+    Console.Write(AkkF(m, n)); 
+}
+
+int AkkF(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkF(m - 1, 1);
+    }
+    else
+    {
+        return (AkkF(m - 1, AkkF(m, n - 1)));
+    }
+}
+
+
+
+
